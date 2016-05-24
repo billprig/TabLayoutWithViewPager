8eade1850d3ad2175705968782e62659dc40a58b
Alexandridis Konstantinos Panagiotis <alexandridis.konpan@hotmail.com>
2016-05-24 14:25:28 +0300

    Changes in wine ,bier

---
+++ app/src/main/java/roy/tablayoutwithviewpager/BeersFragment.java
@@ -0,0 +1,31 @@
+package roy.tablayoutwithviewpager;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BeersFragment extends Fragment {
+
+
+    public BeersFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_beers, container, false);
+    }
+
+
+
+}
