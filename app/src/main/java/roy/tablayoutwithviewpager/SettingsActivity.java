d3541450d7710b6658953f2a47528c769d9fa846
billprig <billprig@yahoo.gr>
2016-05-26 21:52:44 +0300

    Settings Page

--- app/src/main/java/roy/tablayoutwithviewpager/SettingsActivity.java
+++ app/src/main/java/roy/tablayoutwithviewpager/SettingsActivity.java
@@ -1,15 +1,29 @@
 package roy.tablayoutwithviewpager;
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 
+import java.util.HashMap;
+
 public class SettingsActivity extends PreferenceActivity {
+
+   private EditTextPreference username;
+   SessionManager session;
 
    @Override
    public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
 
-      addPreferencesFromResource(R.xml.settings);
+      session = new SessionManager(getApplicationContext());
+      HashMap<String, String> user = session.getUserDetails();
 
+      // name
+      String name = user.get(SessionManager.KEY_NAME);
+
+      addPreferencesFromResource(R.xml.settings);
+      username = (EditTextPreference) findPreference("prefUsername");
+      username.setTitle("Username: "+name);
+      username.setText(name);
    }
 }
