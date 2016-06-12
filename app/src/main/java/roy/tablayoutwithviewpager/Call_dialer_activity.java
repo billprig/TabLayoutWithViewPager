91754a893690585284cd6f6de73f11dc43cd9b52
Alexandridis Konstantinos Panagiotis <alexandridis.konpan@hotmail.com>
2016-06-12 19:06:05 +0300

    CloudServices
    Added notification when accesing call_dialer_activity

--- app/src/main/java/roy/tablayoutwithviewpager/Call_dialer_activity.java
+++ app/src/main/java/roy/tablayoutwithviewpager/Call_dialer_activity.java
@@ -6,7 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.microsoft.windowsazure.mobileservices.*;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
+
+import java.net.MalformedURLException;
+
 public class Call_dialer_activity extends AppCompatActivity {
+
+
+   private MobileServiceClient mClient;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,28 @@ public class Call_dialer_activity extends AppCompatActivity {
          @Override
          public void onClick(View v) {
             goback();
+         }
+      });
+
+      try {
+         mClient = new MobileServiceClient(
+                 "https://alqulator.azurewebsites.net",
+                 this
+         );
+      } catch (MalformedURLException e) {
+         e.printStackTrace();
+      }
+
+
+      TodoItem item = new TodoItem();
+      item.Text = "Awesome item";
+      mClient.getTable(TodoItem.class).insert(item, new TableOperationCallback<TodoItem>() {
+         public void onCompleted(TodoItem entity, Exception exception, ServiceFilterResponse response) {
+            if (exception == null) {
+               // Insert succeeded
+            } else {
+               // Insert failed
+            }
          }
       });
 
---
+++ app/src/main/java/roy/tablayoutwithviewpager/TodoItem.java
@@ -0,0 +1,10 @@
+package roy.tablayoutwithviewpager;
+
+/**
+ * Created by Kostas on 12-Jun-16.
+ */
+public class TodoItem {
+
+    public String Id;
+    public String Text;
+}
